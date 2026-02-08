7409aa6558bbbd99864252927bee4c4b92fdfb7c
Rob Mellett <dev@robmellett.com>
2026-02-08 17:06:56 +0700

    Configure the project for workers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { HTTPException } from "hono/http-exception";
+
+const api = new Hono().get("/health", (c) => {
+	return c.json({ status: "ok" });
+});
 
 const app = new Hono()
+	.get("/", (c) => {
+		return c.text("Hello, Hono!");
+	})
+	.use("/api/*", cors())
+	.route("/api", api);
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.onError((error, c) => {
+	console.error(error);
 
-export default app
+	if (error instanceof HTTPException) {
+		return c.json(
+			{
+				message: error.message,
+				errors: error.cause,
+			},
+			error.status,
+		);
+	}
+
+	return c.json(
+		{
+			message: "Something went wrong",
+		},
+		500,
+	);
+});
+
+export default app;
