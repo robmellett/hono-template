11b200ea21665bfcd1910f34717c565b4be994ce
Rob Mellett <dev@robmellett.com>
2026-05-15 15:27:45 +0700

    Refactor Vitest configuration to leverage new plugin API

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,34 +1,35 @@
-import path from "node:path";
+import { join } from "node:path";
 import {
-	defineWorkersConfig,
-	readD1Migrations,
-} from "@cloudflare/vitest-pool-workers/config";
+  cloudflareTest,
+  readD1Migrations,
+} from "@cloudflare/vitest-pool-workers";
+import { defineConfig } from "vitest/config";
 
-export default defineWorkersConfig(async () => {
-	/**
-	 * Read migrations file to set as test-only environment variable. Used
-	 * in `/tests/setup.ts` to apply migrations before tests run.
-	 * @see https://github.com/cloudflare/workers-sdk/tree/main/fixtures/vitest-pool-workers-examples/d1
-	 */
-	const migrationsPath = path.join(__dirname, "drizzle/migrations");
-	const migrations = await readD1Migrations(migrationsPath);
+export default defineConfig(async () => {
+  /**
+   * Read migrations file to set as test-only environment variable. Used
+   * in `/tests/setup.ts` to apply migrations before tests run.
+   * @see https://github.com/cloudflare/workers-sdk/tree/main/fixtures/vitest-pool-workers-examples/d1
+   */
+  const migrationsPath = join(import.meta.dirname, "drizzle/migrations");
+  const migrations = await readD1Migrations(migrationsPath);
 
-	return {
-		test: {
-			// Enable this line if you need to run D1 migrations before tests
-			// setupFiles: ["./tests/setup.ts"],
-			globals: true,
-			poolOptions: {
-				workers: {
-					wrangler: { configPath: "./wrangler.toml" },
-					miniflare: {
-						compatibilityFlags: ["nodejs_compat"],
-						compatibilityDate: "2024-04-01",
-						d1Databases: ["DB"],
-						bindings: { TEST_MIGRATIONS: migrations },
-					},
-				},
-			},
-		},
-	};
+  return {
+    plugins: [
+      cloudflareTest({
+        wrangler: { configPath: "./wrangler.toml" },
+        miniflare: {
+          compatibilityFlags: ["nodejs_compat"],
+          compatibilityDate: "2024-04-01",
+          d1Databases: ["DB"],
+          bindings: { TEST_MIGRATIONS: migrations },
+        },
+      }),
+    ],
+    test: {
+      // Enable this line if you need to run D1 migrations before tests
+      // setupFiles: ["./tests/setup.ts"],
+      globals: true,
+    },
+  };
 });
